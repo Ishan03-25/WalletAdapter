5d9696029a3a0f8d29e1354468155c7c86f5a08e
Ishan03-25 <ishan03sharma25@gmail.com>
2025-05-05 16:16:44 +0530

    Connection and Wallet Adapter UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
-import './App.css'
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import "./App.css";
+import {
+  WalletDisconnectButton,
+  WalletModalProvider,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+// import { useMemo } from 'react'
+import { clusterApiUrl } from "@solana/web3.js";
+import { useMemo } from "react";
+
+// export const Wallet = ({children}) => {
+//   const network = WalletAdapterNetwork.Devnet;
+
+//   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+//   const wallets = useMemo(()=>[], [network])
+// }
 
 function App() {
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
   return (
-    <>
-      Hi There
-    </>
-  )
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>
+          <div style={{display: "flex", justifyContent: "space-between"}}>
+            <WalletMultiButton />
+            <WalletDisconnectButton />
+          </div>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
 }
 
-export default App
+export default App;
